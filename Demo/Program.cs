e2eaef676c4136ebb2970fcbcf26ac24ead7e268
Bip901 <74911767+Bip901@users.noreply.github.com>
2024-04-28 09:51:50 +0300

    Add Demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using NotificationIcon.NET;
+using System.Runtime.InteropServices;
+
+namespace Demo
+{
+    internal static partial class Program
+    {
+        static void Main()
+        {
+            string iconPath = AppContext.BaseDirectory;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                iconPath = Path.Join(iconPath, "icon.ico");
+            }
+            else
+            {
+                iconPath = Path.Join(iconPath, "icon.png");
+            }
+            NotifyIcon icon = NotifyIcon.Create(iconPath, new List<MenuItem>()
+            {
+                new MenuItem("Example Button"),
+                new MenuItem("Example Checkbox")
+                {
+                    IsChecked = true,
+                    Click = (s, e) =>
+                    {
+                        MenuItem me = (MenuItem)s!;
+                        me.IsChecked = !me.IsChecked;
+                    }
+                },
+                new MenuItem("Quit")
+                {
+                    Click = (s, e) =>
+                    {
+                        e.Icon.Dispose();
+                    }
+                }
+            });
+            icon.Show();
+        }
+    }
+}
